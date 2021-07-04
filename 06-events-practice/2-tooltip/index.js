aff6e4b651b2737ae8de8bf01c982a82727413d0
VitaliSantalau <v.santalau@gmail.com>
2021-07-05 02:17:15 +0300

    solutions 06-events-practice

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,5 +1,63 @@
+/* eslint-disable curly */
 class Tooltip {
+  static instance;  
+  
+  constructor() {
+    if (Tooltip.instance) return Tooltip.instance;
+    Tooltip.instance = this;
+  }
 
+  getTemplate(tooltip) {
+    return `
+      <div class="tooltip">${tooltip}</div>
+    `;
+  }
+
+  render(tooltip) {
+    const element = document.createElement('div');
+    element.innerHTML = this.getTemplate(tooltip);
+    this.element = element.firstElementChild;
+
+    document.body.append(this.element);
+  }
+
+  getHandler(typeEvent) {
+    const handlers = {
+      'pointerover': event => {
+        const tooltip = event.target.dataset.tooltip;
+        if (tooltip) {
+          this.render(tooltip);
+          document.addEventListener('pointermove', this.getHandler('pointermove'));
+          document.addEventListener('pointerout', this.getHandler('pointerout'));  
+        }
+      },
+      'pointermove': event => {
+        this.element.style.left = `${event.clientX + 5}px`;
+        this.element.style.top = `${event.clientY + 5}px`;
+      },
+      'pointerout': () => {
+        this.remove();
+        document.removeEventListener('pointermove', this.getHandler('pointermove'));
+        document.removeEventListener('pointerout', this.getHandler('pointerout'));
+      },
+    };
+
+    return handlers[typeEvent];
+  }
+
+  initialize() {
+    document.addEventListener('pointerover', this.getHandler('pointerover'));
+  }
+
+  remove() {
+    if (this.element) this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    document.removeEventListener('pointerover', this.getHandler('pointerover'));
+  }
 }
 
 export default Tooltip;
